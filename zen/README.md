9bcb11352311f5c4f822f716801251dbe1fc192c
ldkohler <lucasdkohler@gmail.com>
2026-02-05 15:03:05 -0500

    [feat]: firefox settings and ui tweaks

---
+++ zen/README.md
@@ -0,0 +1,3 @@
+# Zen Browser Configs
+
+To-do: Investigate about:telemetry
---
+++ zen/user.js
@@ -0,0 +1,48 @@
+/* Zen value: true
+ * Disables weather display on new tab page - improves privacy by preventing location tracking */
+user_pref("browser.newtabpage.activity-stream.showWeather", false);
+
+/* Zen value: false
+ * Enables the "End Private Session" button during private browsing */
+user_pref("browser.privatebrowsing.resetPBM.enabled", true);
+
+/* Zen value: 15000
+ * Sets session save interval to 60 seconds - reduces disk writes, balancing data preservation with performance */
+user_pref("browser.sessionstore.interval", 60000);
+
+/* Zen value: 0
+ * Enables automatic cookie banner handling. Attempts to reject cookies where possible and ignores other types of banners */
+user_pref("cookiebanners.service.mode", 1);
+
+/* Zen value: 0
+ * Enables automatic cookie banner handling for private browsing sessions. Attempts to reject cookies where possible and ignores other types of banners */
+user_pref("cookiebanners.service.mode.privateBrowsing", 1);
+
+/* Zen value: 256
+ * Increases canvas cache size - improves performance for graphics-intensive pages */
+user_pref("gfx.canvas.accelerated.cache-size", 512);
+
+/* Zen value: 80
+ * Decreases font cache size - conserves memory usage */
+user_pref("gfx.content.skia-font-cache-size", 20);
+
+/* Zen value: 0
+ * Blocks all websites from sending desktop notifications by default (2=block) - improves privacy */
+user_pref("permissions.default.desktop-notification", 2);
+
+/* Zen value: 0
+ * Blocks all websites from accessing location by default (2=block) - improves privacy */
+user_pref("permissions.default.geo", 2);
+
+/* Zen value: 3
+ * Enables CRLite in enforcing mode (2=enforce) - improves privacy and performance of certificate checks */
+user_pref("security.pki.crlite_mode", 2);
+
+/* Zen value: true
+ * Disables capturing and offering to save passwords in private sessions - improves privacy */
+user_pref("signon.privateBrowsingCapture.enabled", false);
+
+/* UI */
+user_pref("zen.tabs.show-newtab-vertical", false);
+user_pref("zen.theme.border-radius", 10);
+user_pref("zen.theme.content-element-separation", 5);
