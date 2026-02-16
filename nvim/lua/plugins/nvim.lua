48c85c74a3d5e63a6a861eb6920bf9bb58ac2c00
ldkohler <lucasdkohler@gmail.com>
2026-02-16 15:23:51 -0500

    [feat]: new formatting plugins and trying neotree

--- nvim/lua/plugins/nvim.lua
+++ nvim/lua/plugins/nvim.lua
@@ -1,24 +1,69 @@
 return {
-{
-    'catppuccin/nvim',
-    name = 'catppuccin',
-    priority = 1000,
-    opts = {
-        flavour = "mocha", -- or "latte" for light mode
-        transparent_background = false,
-        integrations = {
-            treesitter = true,
-            native_lsp = {
-                enabled = true,
+    {
+        'echasnovski/mini.pairs',
+        config = true,
+        event = 'InsertEnter',
+    },
+    {
+        'nvim-neo-tree/neo-tree.nvim',
+        branch = 'v3.x',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            'MunifTanjim/nui.nvim',
+            'nvim-tree/nvim-web-devicons', -- optional, but recommended
+        },
+        lazy = false, -- neo-tree will lazily load itself
+        keys = {
+            { '-', '<cmd>Neotree toggle<cr>', desc = 'Toggle file explorer' },
+        },
+        opts = {
+            filesystem = {
+                follow_current_file = { enabled = true },
+                filtered_items = {
+                    visible = true,
+                    hide_dotfiles = false,
+                    hide_gitignored = false,
+                },
             },
-            telescope = true,
-            cmp = true,
-            gitsigns = true,
+            window = {
+                width = 30,
+            },
         },
     },
-    config = function(_, opts)
-        require("catppuccin").setup(opts)
-        vim.cmd.colorscheme("catppuccin")
-    end,
-},
+    { 'tpope/vim-sleuth', event = 'BufReadPost' },
+    {
+        'kylechui/nvim-surround',
+        config = true,
+        keys = {
+            { 'cs', mode = 'n' },
+            { 'ds', mode = 'n' },
+            { 'ys', mode = 'n' },
+            { 'yS', mode = 'n' },
+            { 'yss', mode = 'n' },
+            { 'ySs', mode = 'n' },
+        },
+    },
+    {
+        'catppuccin/nvim',
+        name = 'catppuccin',
+        priority = 1000,
+        opts = {
+            flavour = 'mocha', -- or "latte" for light mode
+            transparent_background = false,
+            integrations = {
+                treesitter = true,
+                native_lsp = {
+                    enabled = true,
+                },
+                telescope = true,
+                cmp = true,
+                gitsigns = true,
+                neo_tree = true,
+            },
+        },
+        config = function(_, opts)
+            require('catppuccin').setup(opts)
+            vim.cmd.colorscheme('catppuccin')
+        end,
+    },
 }
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,9 +1,52 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  opts = {
-    auto_install = true,
-    highlight = { enable = true },
-    indent = { enable = true },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    opts = {
+      auto_install = true,
+      highlight = { enable = true },
+      indent = { enable = true },
+    },
   },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    config = function()
+      require("nvim-treesitter").setup({
+        textobjects = {
+          select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+              ["af"] = "@function.outer",
+              ["if"] = "@function.inner",
+              ["ac"] = "@class.outer",
+              ["ic"] = "@class.inner",
+              ["aa"] = "@parameter.outer",
+              ["ia"] = "@parameter.inner",
+            },
+          },
+          move = {
+            enable = true,
+            set_jumps = true,
+            goto_next_start = {
+              ["]m"] = "@function.outer",
+              ["]]"] = "@class.outer",
+            },
+            goto_previous_start = {
+              ["[m"] = "@function.outer",
+              ["[["] = "@class.outer",
+            },
+          },
+        },
+      })
+    end,
+  },
+  {
+        'Wansmer/treesj',
+        config = true,
+	keys = {
+            { 'gt', '<cmd>lua require("treesj").toggle()<cr>' },
+    },
+    },
 }
